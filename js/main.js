d62c4547e9333b8c85774d5a9ed41ed1c0912734
KhusniddinovUz <khusniddinovuz@gmail.com>
2025-05-22 23:27:21 +0200

    Make slider autoplay in loop, add circular-loop to all slides

--- js/main.js
+++ js/main.js
@@ -37,8 +37,8 @@
         Hero Slider JS
     =================================*/
     $(".hero-slider").owlCarousel({
-      loop: false,
-      autoplay: false,
+      loop: true,
+      autoplay: true,
       smartSpeed: 500,
       autoplayTimeout: 3500,
       singleItem: true,
